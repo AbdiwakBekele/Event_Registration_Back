ecd1c5f24f33ead00708850826650a1d700ee4cd
Eyoatam Tamirat <eyoatam.tamirat@gmail.com>
2022-11-12 17:11:37 +0300

    some more updates

--- services/database.ts
+++ services/database.ts
@@ -4,6 +4,7 @@ import * as dotenv from "dotenv";
 export const collections: {
   users?: mongoDB.Collection;
   events?: mongoDB.Collection;
+  organization?: mongoDB.Collection;
 } = {};
 
 export async function connectToDB() {
@@ -16,6 +17,23 @@ export async function connectToDB() {
 
   await client.connect();
   const db: mongoDB.Db = client.db(process.env.DB_NAME);
+
+  const usersCollection: mongoDB.Collection = db.collection(
+    process.env.USERS_COLLECTION_NAME
+  );
+
+  collections.users = usersCollection;
+
+  const eventsCollection: mongoDB.Collection = db.collection(
+    process.env.EVENTS_COLLECTION_NAME
+  );
+
+  collections.events = eventsCollection;
+
+  const organizationCollection: mongoDB.Collection = db.collection(
+    process.env.ORGANIZATION_COLLECTION_NAME
+  );
+  collections.organization = organizationCollection;
 
   console.log(`Successfully connected to database: ${db.databaseName}`);
 }
